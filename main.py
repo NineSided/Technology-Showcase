a3f7cdb0b85eac3d72da61d9cadca3b33db456ba
NineSided <Ninesideddevelopment@gmail.com>
2024-10-19 21:31:40 +0100

    Option to turn off shaders in example script

--- main.py
+++ main.py
@@ -1,25 +1,37 @@
-import pygame, sys, math, moderngl, random
+SHADERS = True
+
+import pygame, sys, math, random
+
+if SHADERS:
+    try:
+        import moderngl
+    except:
+        pass
 
 from pygame.locals import *
 
 pygame.init()
 
-from framework.moderngl import Shaders
+if SHADERS:
+    from framework.moderngl import Shaders
 from framework.vfx import Particles
-
 # CONFIG
 
 # SETUP
 clock = pygame.time.Clock()
 
 window_size = [800, 500]
-window = pygame.display.set_mode(window_size, pygame.OPENGL | pygame.DOUBLEBUF)
+if SHADERS:
+    window = pygame.display.set_mode(window_size, pygame.OPENGL | pygame.DOUBLEBUF)
+elif not SHADERS:
+    window = pygame.display.set_mode(window_size)
 pygame.display.set_caption("Technologies Showcase")
 
 surface = pygame.Surface(window_size)
 
 # MAIN
-gameplay_shader = Shaders.Shader()
+if SHADERS:
+    gameplay_shader = Shaders.Shader()
 
 g = Particles.ParticleGenerator(pos=pygame.mouse.get_pos(), p=None, color_=[random.randint(200, 255), random.randint(100, 155), 0])
 
@@ -47,18 +59,26 @@ while 1:
             pygame.quit()
             sys.exit()
 
-    # opengl rendering
-    gameplay_shader.update()
+    # rendering
+    if SHADERS:
+        try:
+            gameplay_shader.update()
 
-    surface1 = pygame.transform.rotate(surface, -30)
-
-    gameplaytex = gameplay_shader.surf_to_texture(surface)
-    gameplaytex.use(0)
-    gameplay_shader.program['tex'] = 0
-    gameplay_shader.render_object.render(mode=moderngl.TRIANGLE_STRIP)
+            gameplaytex = gameplay_shader.surf_to_texture(surface)
+            gameplaytex.use(0)
+            gameplay_shader.program['tex'] = 0
+            gameplay_shader.render_object.render(mode=moderngl.TRIANGLE_STRIP)
+        except:
+            window.blit(surface, (0, 0))
+    elif not SHADERS:
+        window.blit(surface, (0, 0))
 
     # framing
     pygame.display.flip()
-    gameplaytex.release()
+    if SHADERS:
+        try:
+            gameplaytex.release()
+        except:
+            pass
     clock.tick(60)
 
