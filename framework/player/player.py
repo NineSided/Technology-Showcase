59dbd3e25f51938c80e3e08d405ec6840cdd0b60
NineSided <Ninesideddevelopment@gmail.com>
2024-10-24 21:32:57 +0100

    pluh

--- framework/player/player.py
+++ framework/player/player.py
@@ -11,7 +11,14 @@ class Player:
 		
 	def update(self, surface):
 		self.move()
+		self.gravity(0.1, 7)
 		pygame.draw.rect(surface, (255, 0, 0), self.rect)
+
+	def gravity(self, increment, cap):
+		self.y_vel += increment
+		if self.y_vel <= cap:
+			self.y_vel = cap
+		self.rect.y += self.y_vel
 		
 	def move(self):
 		if self.controls.input_connected():
